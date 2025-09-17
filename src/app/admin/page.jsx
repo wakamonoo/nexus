2fed7a745ddc3a3fc8915ad0bc84ca8ad5129652
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-17 15:08:09 +0800

    admin page updates

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -6,10 +6,14 @@ import CountUp from "react-countup";
 
 export default function Page() {
   const [data, setData] = useState({
-    image: "",
     title: "",
+    image: null,
     date: "",
     timeline: "",
+    phase: "",
+    director: "",
+    order: "",
+    url: "",
     summary: "",
   });
   const fileRef = useRef();
@@ -51,10 +55,14 @@ export default function Page() {
         body: JSON.stringify({ ...data, image: imageURL }),
       });
       setData({
-        image: "",
         title: "",
+        image: null,
         date: "",
         timeline: "",
+        phase: "",
+        director: "",
+        order: "",
+        url: "",
         summary: "",
       });
       fileRef.current.value = "";
@@ -83,61 +91,125 @@ export default function Page() {
     <div className="p-4">
       <FaAngleLeft onClick={() => router.push("/")} className="text-2xl" />
 
-      <div className="flex gap-2 items-center justify-center">
-        <div className="w-32 h-32 rounded-full bg-accent flex justify-center items-center border-7 border-white">
-          <CountUp end={count.usersCount} duration={1.5} />
+      <div className="flex gap-2 pt-4 items-center justify-center">
+        <div className="flex items-center gap-2 justify-center">
+          <div className="flex items-center justify-center border-2 border-accent w-24 h-24 rounded-full">
+            <CountUp
+              end={count.usersCount}
+              duration={1.5}
+              className="text-7xl font-extrabold text-accent"
+            />
+          </div>
+          <p className="text-base font-extrabold">USERS</p>
         </div>
 
-        <div className="w-32 h-32 rounded-full bg-accent flex justify-center items-center border-7 border-white">
-          <CountUp end={count.titlesCount} duration={1.5} />
+        <div className="flex items-center gap-2 justify-center">
+          <div className="flex items-center justify-center border-2 border-accent w-24 h-24 rounded-full">
+            <CountUp
+              end={count.titlesCount}
+              duration={1.5}
+              className="text-7xl font-extrabold text-accent"
+            />
+          </div>
+          <p className="text-base font-extrabold">TITLES</p>
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-center gap-4 w-full p-8">
-        <input
-          type="file"
-          name="image"
-          ref={fileRef}
-          onChange={handleChange}
-          className="bg-amber-200 p-4 rounded w-full"
-        />
+      <div className="flex flex-col items-start justify-center gap-4 w-full pt-8">
+        <h1 className="text-2xl">ADD NEW TITLE</h1>
         <input
           type="text"
           name="title"
           value={data.title}
           onChange={handleChange}
           placeholder="Enter Title"
-          className="bg-amber-200 p-4 rounded w-full"
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
         />
+        <div className="flex gap-2 items-center w-full">
+          <label className="text-normal font-normal text-base w-[20%]">
+            Poster
+          </label>
+          <input
+            type="file"
+            name="image"
+            ref={fileRef}
+            onChange={handleChange}
+            className="bg-panel p-4 rounded w-[80%]"
+          />
+        </div>
+
+        <div className="flex items-center w-full">
+          <label className="text-normal font-normal text-base w-[20%]">
+            Release Date
+          </label>
+          <input
+            type="date"
+            name="date"
+            value={data.date}
+            onChange={handleChange}
+            placeholder="Release Date"
+            className="bg-panel text-base text-normal font-normal p-4 rounded w-[80%]"
+          />
+        </div>
         <input
-          type="date"
-          name="date"
-          value={data.date}
-          onChange={handleChange}
-          placeholder="Release Date"
-          className="bg-amber-200 p-4 rounded w-full"
-        />
-        <input
-          type="text"
+          type="number"
           name="timeline"
           value={data.timeline}
           onChange={handleChange}
-          placeholder="Timeline"
-          className="bg-amber-200 p-4 rounded w-full"
+          placeholder="Timeline Date"
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
+        />
+        <select
+          name="phase"
+          value={data.phase}
+          onChange={handleChange}
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
+        >
+          <option value="null">Select Phase</option>
+          <option value="Phase 1">Phase 1</option>
+          <option value="Phase 2">Phase 2</option>
+          <option value="Phase 3">Phase 3</option>
+          <option value="Phase 4">Phase 4</option>
+          <option value="Phase 5">Phase 5</option>
+          <option value="Phase 6">Phase 6</option>
+        </select>
+        <input
+          type="text"
+          name="director"
+          value={data.director}
+          onChange={handleChange}
+          placeholder="Director/s"
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
+        />
+        <input
+          type="number"
+          name="order"
+          value={data.order}
+          onChange={handleChange}
+          placeholder="Order in Timeline"
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
+        />
+        <input
+          type="text"
+          name="url"
+          value={data.url}
+          onChange={handleChange}
+          placeholder="Trailer Url"
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
         />
         <input
           type="text"
           name="summary"
           value={data.summary}
           onChange={handleChange}
-          placeholder="Summary"
-          className="bg-amber-200 p-4 rounded w-full"
+          placeholder="Synopsis"
+          className="bg-panel text-base text-normal font-normal p-4 rounded w-full"
         />
         <button
           onClick={handleAddNewTitle}
-          className="bg-red-500 p-4 w-full rounded"
+          className="bg-accent p-4 w-full rounded font-bold cursor-pointer"
         >
-          submit
+          <p className="text-base">Submit</p>
         </button>
       </div>
     </div>
