144f23e476b38434f8f87b37a790f8d0e67c891e
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-19 00:41:50 +0800

    ui updates

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
- apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -16,17 +16,17 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export const googleSignUp = async() => {
+export const googleSignUp = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
-    const token = await user.getIdToken();
-    
+    const token = await user.getIdToken(true);
+
     return { user, token, error: null };
   } catch (error) {
-    console.error("signin failed:", error)
-    return { user: null, token: null, error }
+    console.error("signin failed:", error);
+    return { user: null, token: null, error };
   }
-}
+};
 
 export { auth };
--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,133 +1,163 @@
 import Banner from "@/components/banner.jsx";
 import { FaBolt, FaComment, FaShare } from "react-icons/fa";
 import Fallback from "@/assets/fallback.png";
+import Tony from "@/assets/tony.jpg";
+import Steve from "@/assets/steve.jpg";
+import Thor from "@/assets/thor.jpg";
+import Wanda from "@/assets/wanda.jpeg";
 import Image from "next/image";
 
 export default function Hero() {
   return (
     <div className="bg-brand w-full pt-16">
-      <Banner />
+      <div className="pb-4">
+        <Banner />
+      </div>
 
-      <div className="p-4 flex flex-col gap-4">
-        <div className="w-full h-auto bg-second rounded p-4">
-          <div className="flex gap-2 items-center py-2">
+      <div className="p-2 flex flex-col gap-4">
+        <div className="w-full h-auto bg-second rounded-tl-4xl border-t-2 border-r-1 border-accent p-4">
+          <div className="flex gap-3 items-center py-2">
             <Image
-              src={Fallback}
+              src={Tony}
               alt="user"
               width={0}
               height={0}
               sizes="100vw"
-              className="w-8 h-auto rounded-full"
+              className="w-12 h-12 object-cover rounded-full"
             />
             <div className="flex flex-col">
-              <p className="text-base mt-2 font-bold leading-2.5">
-                Joven Bataller
-              </p>
-              <p className="text-xs text-gray-400">06-17-2003</p>
+              <p className="text-base mt-2 font-bold leading-3.5">Tony Stark</p>
+              <p className="text-xs text-vibe">06-17-2003</p>
             </div>
           </div>
-          <p className="text-base text-justify">
-            The proper way to fix this is to give your content a top padding
-            equal to the NavBar’s height (or a bit more if you want extra
-            space), rather than adding margin to the Banner. This keeps the
-            NavBar fixed and independent of the content.
+
+          <p className="text-base text-justify py-4">
+            Just finished tinkering with a new suit upgrade — let’s just say
+            Friday might need a bigger hard drive. Who’s up for a test flight
+            over Wakanda?
           </p>
-          <div className="flex justify-between items-center p-2 border-t mt-2">
-            <FaBolt className="text-xl" />
-            <FaComment className="text-xl" />
-            <FaShare className="text-xl" />
+          <div className="flex justify-between items-center pt-4 border-t gap-4 mt-2">
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaBolt className="text-xl" />
+              <p className="text-xs font-light text-vibe">21</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaComment className="text-xl" />
+              <p className="text-xs font-light text-vibe">21</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaShare className="text-xl" />
+              <p className="text-xs font-light text-vibe">21</p>
+            </div>
           </div>
         </div>
 
-        <div className="w-full h-auto bg-second rounded p-4">
-          <div className="flex gap-2 items-center py-2">
+        <div className="w-full h-auto bg-second rounded-tl-4xl border-t-2 border-r-1 border-accent p-4">
+          <div className="flex gap-3 items-center py-2">
             <Image
-              src={Fallback}
+              src={Steve}
               alt="user"
               width={0}
               height={0}
               sizes="100vw"
-              className="w-8 h-auto rounded-full"
+              className="w-12 h-12 object-cover rounded-full"
             />
             <div className="flex flex-col">
-              <p className="text-base mt-2 font-bold leading-2.5">
-                Joven Bataller
-              </p>
-              <p className="text-xs text-gray-400">06-17-2003</p>
+              <p className="text-base mt-2 font-bold leading-3.5">Cap</p>
+              <p className="text-xs text-vibe">06-17-2003</p>
             </div>
           </div>
-          <p className="text-base text-justify">
-            The proper way to fix this is to give your content a top padding
-            equal to the NavBar’s height (or a bit more if you want extra
-            space), rather than adding margin to the Banner. This keeps the
-            NavBar fixed and independent of the content.
+
+          <p className="text-base text-justify py-4">
+            Morning run complete. Remember: strength comes from unity. Grab your
+            shield and stand for what’s right, friends.
           </p>
-          <div className="flex justify-between items-center p-2 border-t mt-2">
-            <FaBolt className="text-xl" />
-            <FaComment className="text-xl" />
-            <FaShare className="text-xl" />
+          <div className="flex justify-between items-center pt-4 border-t gap-4 mt-2">
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaBolt className="text-xl" />
+              <p className="text-xs font-light text-vibe">101</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaComment className="text-xl" />
+              <p className="text-xs font-light text-vibe">211</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaShare className="text-xl" />
+              <p className="text-xs font-light text-vibe">501</p>
+            </div>
           </div>
         </div>
 
-        <div className="w-full h-auto bg-second rounded p-4">
-          <div className="flex gap-2 items-center py-2">
+        <div className="w-full h-auto bg-second rounded-tl-4xl border-t-2 border-r-1 border-accent p-4">
+          <div className="flex gap-3 items-center py-2">
             <Image
-              src={Fallback}
+              src={Thor}
               alt="user"
               width={0}
               height={0}
               sizes="100vw"
-              className="w-8 h-auto rounded-full"
+              className="w-12 h-12 object-cover rounded-full"
             />
             <div className="flex flex-col">
-              <p className="text-base mt-2 font-bold leading-2.5">
-                Joven Bataller
-              </p>
-              <p className="text-xs text-gray-400">06-17-2003</p>
+              <p className="text-base mt-2 font-bold leading-3.5">Stronest Avenger</p>
+              <p className="text-xs text-vibe">06-17-2003</p>
             </div>
           </div>
-          <p className="text-base text-justify">
-            The proper way to fix this is to give your content a top padding
-            equal to the NavBar’s height (or a bit more if you want extra
-            space), rather than adding margin to the Banner. This keeps the
-            NavBar fixed and independent of the content.
+
+          <p className="text-base text-justify py-4">
+            A thunderous storm approaches! I suggest all Asgardians and Midgardians brace themselves. Perhaps someone fancies a flight across the Bifrost?
           </p>
-          <div className="flex justify-between items-center p-2 border-t mt-2">
-            <FaBolt className="text-xl" />
-            <FaComment className="text-xl" />
-            <FaShare className="text-xl" />
+          <div className="flex justify-between items-center pt-4 border-t gap-4 mt-2">
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaBolt className="text-xl" />
+              <p className="text-xs font-light text-vibe">311</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaComment className="text-xl" />
+              <p className="text-xs font-light text-vibe">451</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaShare className="text-xl" />
+              <p className="text-xs font-light text-vibe">59</p>
+            </div>
           </div>
         </div>
 
-        <div className="w-full h-auto bg-second rounded p-4">
-          <div className="flex gap-2 items-center py-2">
+        <div className="w-full h-auto bg-second rounded-tl-4xl border-t-2 border-r-1 border-accent p-4">
+          <div className="flex gap-3 items-center py-2">
             <Image
-              src={Fallback}
+              src={Wanda}
               alt="user"
               width={0}
               height={0}
               sizes="100vw"
-              className="w-8 h-auto rounded-full"
+              className="w-12 h-12 object-cover rounded-full"
             />
             <div className="flex flex-col">
-              <p className="text-base mt-2 font-bold leading-2.5">
-                Joven Bataller
-              </p>
-              <p className="text-xs text-gray-400">06-17-2003</p>
+              <p className="text-base mt-2 font-bold leading-3.5">Wanda</p>
+              <p className="text-xs text-vibe">06-17-2003</p>
             </div>
           </div>
-          <p className="text-base text-justify">
-            The proper way to fix this is to give your content a top padding
-            equal to the NavBar’s height (or a bit more if you want extra
-            space), rather than adding margin to the Banner. This keeps the
-            NavBar fixed and independent of the content.
+
+          <p className="text-base text-justify py-4">
+            Magic is only as dangerous as the heart that wields it… and mine is on fire. 🔥 Anyone else feel the chaos tonight?
           </p>
-          <div className="flex justify-between items-center p-2 border-t mt-2">
-            <FaBolt className="text-xl" />
-            <FaComment className="text-xl" />
-            <FaShare className="text-xl" />
+          <div className="flex justify-between items-center pt-4 border-t gap-4 mt-2">
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaBolt className="text-xl" />
+              <p className="text-xs font-light text-vibe">1023</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaComment className="text-xl" />
+              <p className="text-xs font-light text-vibe">981</p>
+            </div>
+            <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+              <FaShare className="text-xl" />
+              <p className="text-xs font-light text-vibe">789</p>
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   );
