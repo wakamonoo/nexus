72169d07aac2351489ca91a9197bbaac6e5fd243
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-20 01:57:29 +0800

    mcu loader

--- src/components/titlesLoader.jsx
+++ src/components/titlesLoader.jsx
@@ -3,7 +3,7 @@ export default function TitlesLoader() {
     <div className="flex flex-col items-start justify-center w-full h-full py-4 gap-8 overflow-hidden">
       <div className="flex flex-col gap-2">
         <div className="animate-pulse bg-second w-36 h-6 rounded-lg" />
-        <div className="flex overflow-hidden gap-2 w-full">
+        <div className="flex gap-2 w-full">
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
@@ -18,7 +18,7 @@ export default function TitlesLoader() {
       </div>
       <div className="flex flex-col gap-2">
         <div className="animate-pulse bg-second w-57 h-6 rounded-lg" />
-        <div className="flex overflow-hidden gap-2 w-full">
+        <div className="flex gap-2 w-full">
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
@@ -47,7 +47,7 @@ export default function TitlesLoader() {
       </div>
       <div className="flex flex-col gap-2">
         <div className="animate-pulse bg-second w-42 h-6 rounded-lg" />
-        <div className="flex overflow-hidden gap-2 w-full">
+        <div className="flex gap-2 w-full">
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
@@ -76,7 +76,7 @@ export default function TitlesLoader() {
       </div> 
       <div className="flex flex-col gap-2">
         <div className="animate-pulse bg-second w-27 h-6 rounded-lg" />
-        <div className="flex overflow-hidden gap-2 w-full">
+        <div className="flex gap-2 w-full">
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
           <div className="animate-pulse bg-second w-26 h-40 rounded-lg" />
