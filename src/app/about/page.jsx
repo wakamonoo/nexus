89906946c21814146a23e958824fcc6f538b755d
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-23 22:53:06 +0800

    adding the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div className="bg-brand w-full p-2 pt-24">asd</div>;
+}
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -5,12 +5,14 @@ import { MdLogin, MdLogout } from "react-icons/md";
 import { UserContext } from "@/context/userContext";
 import { ScrollContext } from "@/context/scrollContext";
 import { MenuContext } from "@/context/menuContext";
+import { useRouter } from "next/navigation";
 
 export default function Menu() {
   const divRef = useRef(null);
   const { user, setShowSignIn } = useContext(UserContext);
   const { navHide } = useContext(ScrollContext);
   const { showMenu, setShowMenu, buttonRef } = useContext(MenuContext);
+  const router = useRouter();
 
   useEffect(() => {
     const handleOutClick = (e) => {
@@ -31,13 +33,19 @@ export default function Menu() {
     <div
       ref={divRef}
       className={`z-[100] bg-accent inset-0 fixed left-0 w-[75%] h-screen top-16 p-8 transition-all duration-300 ${
-        showMenu ? "translate-x-0" : "translate-x-[-100%]" 
+        showMenu ? "translate-x-0" : "translate-x-[-100%]"
       } ${
         navHide ? "translate-x-[-100%] opacity-0" : "translate-x-0 opacity-100"
       }`}
     >
       <div className="flex flex-col gap-2 items-start">
-        <button className="flex items-center cursor-pointer gap-4 hover:bg-[var(--color-text)] focus:bg-[var(--color-text)] group w-full p-4 rounded-full transition-all duration-300">
+        <button
+          onClick={() => {
+            router.push("/about");
+            setShowMenu(false);
+          }}
+          className="flex items-center cursor-pointer gap-4 hover:bg-[var(--color-text)] focus:bg-[var(--color-text)] group w-full p-4 rounded-full transition-all duration-300"
+        >
           <FaInfo className="text-2xl group-hover:text-[var(--color-accent)] group-focus:text-[var(--color-accent)]" />
           <p className="text-base flex font-bold group-hover:text-[var(--color-accent)] group-focus:text-[var(--color-accent)]">
             About
