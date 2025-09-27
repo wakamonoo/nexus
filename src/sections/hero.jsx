7c7315c6f41a293421eaf782848019af967c1c99
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-27 23:34:14 +0800

    showInfo addition for mor seamless browsing

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -15,6 +15,7 @@ export default function Hero() {
   const [lightboxOpen, setLightboxOpen] = useState(false);
   const [currentPost, setCurrentPost] = useState([]);
   const [currentPostInfo, setCurrentPostInfo] = useState(null);
+  const [showInfo, setShowInfo] = useState(true);
   const [initialIndex, setInitialIndex] = useState(0);
   const lightboxRef = useRef();
 
@@ -156,7 +157,7 @@ export default function Hero() {
             <MdClose onClick={() => setLightboxOpen(false)} />
           </button>
           <div
-            onClick={(e) => e.stopPropagation()}
+            onClick={(e) => {e.stopPropagation(); setShowInfo((prev) => !prev)}}
             ref={lightboxRef}
             className="flex items-center bg-brand overflow-x-auto snap-x snap-mandatory scroll-smooth relative w-[100%] h-[100%] scrollbar-hide"
           >
@@ -188,31 +189,33 @@ export default function Hero() {
               );
             })}
           </div>
-          <div className="absolute bg-[var(--color-secondary)]/50 w-full p-2 bottom-0 left-0">
-            <div className="flex flex-col">
-              <p className="text-base mt-2 font-bold leading-3.5">
-                {currentPostInfo.userName}
-              </p>
-              <p className="text-xs text-vibe">{currentPostInfo.date}</p>
-            </div>
-            <div>
-              <p className="text-base text-normal">{currentPostInfo.text}</p>
-            </div>
-            <div className="flex justify-between items-center pt-4 border-t gap-4 mt-2">
-              <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
-                <FaBolt className="text-xl" />
-                <p className="text-xs font-light text-vibe">21</p>
+          {showInfo && (
+            <div className="absolute bg-[var(--color-secondary)]/50 w-full p-2 bottom-0 left-0">
+              <div className="flex flex-col">
+                <p className="text-base mt-2 font-bold leading-3.5">
+                  {currentPostInfo.userName}
+                </p>
+                <p className="text-xs text-vibe">{currentPostInfo.date}</p>
               </div>
-              <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
-                <FaComment className="text-xl" />
-                <p className="text-xs font-light text-vibe">21</p>
+              <div>
+                <p className="text-base text-normal">{currentPostInfo.text}</p>
               </div>
-              <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
-                <FaShare className="text-xl" />
-                <p className="text-xs font-light text-vibe">21</p>
+              <div className="flex justify-between items-center pt-4 border-t gap-4 mt-2">
+                <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+                  <FaBolt className="text-xl" />
+                  <p className="text-xs font-light text-vibe">21</p>
+                </div>
+                <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+                  <FaComment className="text-xl" />
+                  <p className="text-xs font-light text-vibe">21</p>
+                </div>
+                <div className="flex items-center justify-center gap-2 border-1 p-4 rounded-4xl w-[33%] h-12">
+                  <FaShare className="text-xl" />
+                  <p className="text-xs font-light text-vibe">21</p>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       )}
     </>
