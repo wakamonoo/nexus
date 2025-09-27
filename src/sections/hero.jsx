e11b9e08994413c0618bec484d66242444d38975
wakamonoo <joven.serdanbataller21@gmail.com>
2025-09-27 23:37:49 +0800

    minor tweaks

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -15,7 +15,7 @@ export default function Hero() {
   const [lightboxOpen, setLightboxOpen] = useState(false);
   const [currentPost, setCurrentPost] = useState([]);
   const [currentPostInfo, setCurrentPostInfo] = useState(null);
-  const [showInfo, setShowInfo] = useState(true);
+  const [showDetails, setShowDetails] = useState(true);
   const [initialIndex, setInitialIndex] = useState(0);
   const lightboxRef = useRef();
 
@@ -153,11 +153,16 @@ export default function Hero() {
       </div>
       {lightboxOpen && (
         <div className="inset-0 z-[100] flex flex-col items-center justify-center fixed">
-          <button className="absolute cursor-pointer z-[120] top-4 right-4 text-2xl sm:text-3xl md:text-4xl font-bold duration-200 hover:scale-110 active:scale-110">
-            <MdClose onClick={() => setLightboxOpen(false)} />
-          </button>
+          {showDetails && (
+            <button className="absolute cursor-pointer z-[120] top-4 right-4 text-2xl sm:text-3xl md:text-4xl font-bold duration-200 hover:scale-110 active:scale-110">
+              <MdClose onClick={() => setLightboxOpen(false)} />
+            </button>
+          )}
           <div
-            onClick={(e) => {e.stopPropagation(); setShowInfo((prev) => !prev)}}
+            onClick={(e) => {
+              e.stopPropagation();
+              setShowDetails((prev) => !prev);
+            }}
             ref={lightboxRef}
             className="flex items-center bg-brand overflow-x-auto snap-x snap-mandatory scroll-smooth relative w-[100%] h-[100%] scrollbar-hide"
           >
@@ -167,7 +172,7 @@ export default function Hero() {
               return (
                 <div
                   key={index}
-                  className="flex-shrink-0 w-full h-[80%] snap-center flex items-center justify-center relative"
+                  className="flex-shrink-0 w-full h-full snap-center flex items-center justify-center relative"
                 >
                   {["jpg", "jpeg", "png", "gif", "webp"].includes(ext) && (
                     <Image
@@ -189,7 +194,7 @@ export default function Hero() {
               );
             })}
           </div>
-          {showInfo && (
+          {showDetails && (
             <div className="absolute bg-[var(--color-secondary)]/50 w-full p-2 bottom-0 left-0">
               <div className="flex flex-col">
                 <p className="text-base mt-2 font-bold leading-3.5">
