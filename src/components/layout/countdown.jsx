a910104dbe873b2af9846b8dde68c8093fba7f2f
wakamonoo <joven.serdanbataller21@gmail.com>
2026-06-21 16:27:53 +0800

    ui update

--- src/components/layout/countdown.jsx
+++ src/components/layout/countdown.jsx
@@ -4,8 +4,10 @@ import { TitleContext } from "@/context/titleContext";
 import { useContext, useEffect, useState } from "react";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
+import utc from "dayjs/plugin/utc";
 
 dayjs.extend(duration);
+dayjs.extend(utc);
 
 export default function Countdown() {
   const { upcomingTitles } = useContext(TitleContext);
@@ -23,7 +25,7 @@ export default function Countdown() {
     ?.filter((title) => new Date(title.date) > now.toDate())
     ?.sort((a, b) => new Date(a.date) - new Date(b.date))[0];
 
-  const target = dayjs(nextOne?.date);
+  const target = dayjs.utc(nextOne?.date + "T02:00:00Z");
   const timeLeft = dayjs.duration(target.diff(now));
 
   const months = timeLeft.months();
@@ -33,37 +35,49 @@ export default function Countdown() {
   const seconds = timeLeft.seconds();
 
   return (
-    <div className="mt-2 px-2 w-full">
+    <div className="mt-2 w-full">
       <div
-        className="border border-panel rounded p-4"
+        className="p-2"
         style={{
           backgroundImage: `url(${nextOne?.image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-        <h1 className="text-xl text-center">Countdown to {nextOne?.title}</h1>
-        <div className="flex flex-wrap items-center justify-center gap-2 w-full mt-4">
-          <div className="w-20 h-20 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+        <h1 className="text-2xl text-center">Countdown to {nextOne?.title}</h1>
+        <div className="flex flex-wrap items-center justify-center gap-2 w-full">
+          <div className="w-14 h-14 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
             <p className="font-alt text-2xl font-bold">{months}</p>
             <p className="text-xs">Months</p>
           </div>
-          <div className="w-20 h-20 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+          <div className="w-14 h-14 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
             <p className="font-alt text-2xl font-bold">{days}</p>
             <p className="text-xs">Days</p>
           </div>
-          <div className="w-20 h-20 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+          <div className="w-14 h-14 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
             <p className="font-alt text-2xl font-bold">{hours}</p>
             <p className="text-xs">Hours</p>
           </div>
-          <div className="w-20 h-20 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+          <div className="w-14 h-14 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
             <p className="font-alt text-2xl font-bold">{minutes}</p>
             <p className="text-xs">Minutes</p>
           </div>
-          <div className="w-20 h-20 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+          <div className="w-14 h-14 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
             <p className="font-alt text-2xl font-bold">{seconds}</p>
             <p className="text-xs">Seconds</p>
           </div>
+        </div>
+        <div className="flex items-center gap-2 px-4">
+          <div className="flex-1 border-t border-vibe" />
+          <p className="text-xs text-center text-vibe mt-2">
+            {new Date(nextOne.date).toLocaleDateString([], {
+              month: "short",
+              day: "2-digit",
+              year: "numeric",
+            })}{" "}
+            GLOBAL RELEASE DATE
+          </p>
+          <div className="flex-1 border-t border-vibe" />
         </div>
       </div>
     </div>
