b71a9b1029d2c3eca3fd3f9413baade6532e01a0
wakamonoo <joven.serdanbataller21@gmail.com>
2026-07-14 15:05:04 +0800

    feat: now shoing alt on countdown added

--- src/components/layout/countdown.jsx
+++ src/components/layout/countdown.jsx
@@ -34,17 +34,27 @@ export default function Countdown() {
     return () => clearInterval(interval);
   }, []);
 
-  const nextOne = upcomingTitles
-    ?.map((title) => ({
-      ...title,
-      dateUTC: dayjs.utc(title.date),
-    }))
+  const titles = upcomingTitles?.map((title) => ({
+    ...title,
+    dateUTC: dayjs.utc(title.date),
+  }));
+
+  const latestRelased = titles
+    ?.filter((title) => title.dateUTC.isBefore(now))
+    ?.sort((a, b) => a.dateUTC.diff(b.dateUTC))[0];
+
+  const nextUpcomming = titles
     ?.filter((title) => title.dateUTC.isAfter(now))
     ?.sort((a, b) => a.dateUTC.diff(b.dateUTC))[0];
 
-  if (!nextOne) return null;
+  const showReleased =
+    latestRelased && now.diff(latestRelased.dateUTC, "day", true) < 7;
 
-  const target = nextOne.dateUTC;
+  const displayTitle = showReleased ? latestRelased : nextUpcomming;
+
+  if (!displayTitle) return null;
+
+  const target = displayTitle.dateUTC;
   const distance = target.diff(now);
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -59,71 +69,87 @@ export default function Countdown() {
       <div
         className="p-2"
         style={{
-          backgroundImage: `url(${nextOne?.image})`,
+          backgroundImage: `url(${displayTitle?.image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-        <h1 className="text-xl text-center">Countdown to {nextOne?.title}</h1>
-        <div className="flex flex-wrap items-center justify-center gap-2 w-full">
-          <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
-            <span className="countdown text-3xl font-bold">
-              <span
-                style={{ "--value": days, "--digits": 2 }}
-                aria-live="polite"
-                aria-label={days}
-              />
-            </span>
-            <p className="text-xs">Days</p>
+        {nextUpcomming && (
+          <>
+            <h1 className="text-xl text-center">
+              Countdown to {displayTitle?.title}
+            </h1>
+            <div className="flex flex-wrap items-center justify-center gap-2 w-full">
+              <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+                <span className="countdown text-3xl font-bold">
+                  <span
+                    style={{ "--value": days, "--digits": 2 }}
+                    aria-live="polite"
+                    aria-label={days}
+                  />
+                </span>
+                <p className="text-xs">Days</p>
+              </div>
+              <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+                <span className="countdown text-3xl font-bold">
+                  <span
+                    style={{ "--value": hours, "--digits": 2 }}
+                    aria-live="polite"
+                    aria-label={hours}
+                  />
+                </span>
+                <p className="text-xs">Hours</p>
+              </div>
+              <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+                <span className="countdown text-3xl font-bold">
+                  <span
+                    style={{ "--value": minutes, "--digits": 2 }}
+                    aria-live="polite"
+                    aria-label={minutes}
+                  />
+                </span>
+                <p className="text-xs">Minutes</p>
+              </div>
+              <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
+                <span className="countdown text-3xl font-bold">
+                  <span
+                    style={{ "--value": seconds, "--digits": 2 }}
+                    aria-live="polite"
+                    aria-label={seconds}
+                  />
+                </span>
+                <p className="text-xs">Seconds</p>
+              </div>
+            </div>
+            <div className="relative w-full flex items-center justify-center gap-2 p-2 mt-2">
+              <p
+                className={`absolute text-xs text-center text-vibe transition-opacity duration-700 ${globalShow ? "opacity-100" : "opacity-0"}`}
+              >
+                <span className="font-bold">UTC: </span>
+                {dayjs.utc(displayTitle?.date).format("MMM DD, YYYY HH:mm")}
+              </p>
+              <p
+                className={`absolute text-xs text-center text-vibe transition-opacity duration-700 ${localShow ? "opacity-100" : "opacity-0"}`}
+              >
+                <span className="font-bold">Local: </span>
+                {dayjs
+                  .utc(displayTitle?.date)
+                  .tz(userTimeZone)
+                  .format("MMM DD, YYYY HH:mm")}{" "}
+              </p>
+            </div>
+          </>
+        )}
+        {latestRelased && (
+          <div className="flex flex-col items-center justify-center leading-none">
+            <h1 className="text-4xl font-bold text-center tracking-tight">
+              {displayTitle?.title}
+            </h1>
+            <p className="uppercase text-sm font-alt text-vibe tracking-[0.35em]">
+              Is now showing
+            </p>
           </div>
-          <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
-            <span className="countdown text-3xl font-bold">
-              <span
-                style={{ "--value": hours, "--digits": 2 }}
-                aria-live="polite"
-                aria-label={hours}
-              />
-            </span>
-            <p className="text-xs">Hours</p>
-          </div>
-          <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
-            <span className="countdown text-3xl font-bold">
-              <span
-                style={{ "--value": minutes, "--digits": 2 }}
-                aria-live="polite"
-                aria-label={minutes}
-              />
-            </span>
-            <p className="text-xs">Minutes</p>
-          </div>
-          <div className="w-18 h-18 flex flex-col items-center justify-center border border-panel rounded bg-[var(--color-panel)]/60 p-4">
-            <span className="countdown text-3xl font-bold">
-              <span
-                style={{ "--value": seconds, "--digits": 2 }}
-                aria-live="polite"
-                aria-label={seconds}
-              />
-            </span>
-            <p className="text-xs">Seconds</p>
-          </div>
-        </div>
-        <div className="relative w-full flex items-center justify-center gap-2 p-2 mt-2">
-          <p
-            className={`absolute text-xs text-center text-vibe transition-opacity duration-700 ${globalShow ? "opacity-100" : "opacity-0"}`}
-          >
-            <span className="font-bold">UTC: </span>
-            {dayjs.utc(nextOne?.date).format("MMM DD, YYYY HH:mm")}
-          </p>
-          <p
-            className={`absolute text-xs text-center text-vibe transition-opacity duration-700 ${localShow ? "opacity-100" : "opacity-0"}`}
-          >
-            <span className="font-bold">Local: </span>
-            {dayjs
-              .utc(nextOne?.date)
-              .tz(userTimeZone)
-              .format("MMM DD, YYYY HH:mm")}{" "}
-          </p>
-        </div>
+        )}
       </div>
     </div>
   );
