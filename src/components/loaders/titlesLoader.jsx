f30b824a7136843523231cf388b819c96030024e
wakamonoo <joven.serdanbataller21@gmail.com>
2026-07-16 22:40:16 +0800

    feat: added loader on hex

--- src/components/loaders/titlesLoader.jsx
+++ src/components/loaders/titlesLoader.jsx
@@ -1,7 +1,10 @@
 export default function TitlesLoader() {
   return (
     <div className="flex flex-col items-start justify-center w-full h-full gap-4 overflow-hidden animate-pulse">
-      <div className="bg-panel w-full h-10 rounded-full" />
+      <div className="flex w-full justify-between items-center gap-2">
+        <div className="bg-panel w-full h-10 rounded-full" />
+        <div className="bg-panel w-34 h-10 rounded-md" />
+      </div>
       <div className="flex flex-col gap-2 pb-4">
         <div className="bg-panel w-36 h-6 rounded" />
         <div className="flex gap-2 w-full">
@@ -122,6 +125,49 @@ export default function TitlesLoader() {
           <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
           <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
           <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 pb-4">
+        <div className="bg-panel w-44 h-6 rounded" />
+        <div className="flex gap-2 w-full">
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+          <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 pb-4">
+        <div className="bg-panel w-42 h-6 rounded" />
+        <div className="flex gap-2 w-full">
           <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
           <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
           <div className="bg-panel w-26 h-40 md:w-32 md:h-46 rounded" />
