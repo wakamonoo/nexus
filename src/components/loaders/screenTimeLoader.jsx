15d9fe866d2830c68db04c408b7343a1f54f4b55
wakamonoo <joven.serdanbataller21@gmail.com>
2026-07-17 23:40:19 +0800

    loader fix

--- src/components/loaders/screenTimeLoader.jsx
+++ src/components/loaders/screenTimeLoader.jsx
@@ -17,43 +17,45 @@ export default function ScreenTimeLoader() {
             <div className="flex flex-col gap-4 mt-4">
               <div className="flex justify-between bg-panel border border-[var(--color-secondary)] rounded-xl p-3">
                 <div className="flex items-center gap-2">
-                  <div className="w-8 h-6 bg-[var(--color-secondary)] rounded flex-shrink-0" />
+                  <div className="w-6 h-6 bg-[var(--color-secondary)] rounded flex-shrink-0" />
                   <div className="w-12 h-12 bg-[var(--color-secondary)] rounded-full flex-shrink-0" />
                   <div className="flex flex-col gap-2">
-                    <div className="w-36 h-6 bg-[var(--color-secondary)] rounded" />
-                    <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
+                    <div className="w-24 h-6 bg-[var(--color-secondary)] rounded" />
+                    <div className="w-12 h-4 bg-[var(--color-secondary)] rounded" />
                   </div>
                 </div>
                 <div className="flex flex-col gap-2 items-end">
-                  <div className="w-36 h-6 bg-[var(--color-secondary)] rounded" />
+                  <div className="w-8 h-6 bg-[var(--color-secondary)] rounded" />
                   <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
                 </div>
               </div>
-               <div className="flex justify-between bg-panel border border-[var(--color-secondary)] rounded-xl p-3">
-                <div className="flex items-center gap-2">
-                  <div className="w-8 h-6 bg-[var(--color-secondary)] rounded flex-shrink-0" />
-                  <div className="w-12 h-12 bg-[var(--color-secondary)] rounded-full flex-shrink-0" />
-                  <div className="flex flex-col gap-2">
-                    <div className="w-36 h-6 bg-[var(--color-secondary)] rounded" />
-                    <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
-                  </div>
-                </div>
-                <div className="flex flex-col gap-2 items-end">
-                  <div className="w-36 h-6 bg-[var(--color-secondary)] rounded" />
-                  <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
-                </div>
-              </div>
+
               <div className="flex justify-between bg-panel border border-[var(--color-secondary)] rounded-xl p-3">
                 <div className="flex items-center gap-2">
-                  <div className="w-8 h-6 bg-[var(--color-secondary)] rounded flex-shrink-0" />
+                  <div className="w-6 h-6 bg-[var(--color-secondary)] rounded flex-shrink-0" />
                   <div className="w-12 h-12 bg-[var(--color-secondary)] rounded-full flex-shrink-0" />
                   <div className="flex flex-col gap-2">
-                    <div className="w-36 h-6 bg-[var(--color-secondary)] rounded" />
-                    <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
+                    <div className="w-20 h-6 bg-[var(--color-secondary)] rounded" />
+                    <div className="w-12 h-4 bg-[var(--color-secondary)] rounded" />
                   </div>
                 </div>
                 <div className="flex flex-col gap-2 items-end">
-                  <div className="w-36 h-6 bg-[var(--color-secondary)] rounded" />
+                  <div className="w-10 h-6 bg-[var(--color-secondary)] rounded" />
+                  <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
+                </div>
+              </div>
+
+              <div className="flex justify-between bg-panel border border-[var(--color-secondary)] rounded-xl p-3">
+                <div className="flex items-center gap-2">
+                  <div className="w-6 h-6 bg-[var(--color-secondary)] rounded flex-shrink-0" />
+                  <div className="w-12 h-12 bg-[var(--color-secondary)] rounded-full flex-shrink-0" />
+                  <div className="flex flex-col gap-2">
+                    <div className="w-28 h-6 bg-[var(--color-secondary)] rounded" />
+                    <div className="w-12 h-4 bg-[var(--color-secondary)] rounded" />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2 items-end">
+                  <div className="w-8 h-6 bg-[var(--color-secondary)] rounded" />
                   <div className="w-24 h-4 bg-[var(--color-secondary)] rounded" />
                 </div>
               </div>
